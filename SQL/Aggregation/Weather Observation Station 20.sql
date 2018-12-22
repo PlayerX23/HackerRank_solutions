ce5d3824d245659d1078b29243982e2f5a825cd6
Player X 23 <46050663+PlayerX23@users.noreply.github.com>
2018-12-22 17:17:00 +0530

    New Weather Observation 20 solution

---
+++ SQL/Aggregation/Weather Observation Station 20.sql
@@ -0,0 +1,5 @@
+--     Author: PlayerX23
+--     Github: github.com/PlayerX23
+
+
+Select round(S.LAT_N,4) mediam from station S where (select count(Lat_N) from station where Lat_N < S.LAT_N ) = (select count(Lat_N) from station where Lat_N > S.LAT_N);
